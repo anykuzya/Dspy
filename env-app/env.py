ec3153390a971012c4f8185a35c761b089598473
Anna.Kuznetsova <Anna.Kuznetsova@jetbrains.com>
2018-05-24 16:41:25 +0300

    fix deliver, add api

--- env-app/env.py
+++ env-app/env.py
@@ -8,6 +8,7 @@ message_counter = 0
 known_execs = set()
 queues = {}
 
+
 @app.route("/messages", methods=['POST'])
 def addMessage():
     global message_counter
@@ -24,9 +25,11 @@ def addMessage():
     message_counter += 1
     return "200"
 
+
 @app.route("/")
 def main_page():
     return send_file('index.html')
+
 
 @app.route("/messages", methods=['GET'])
 def getMessages():
@@ -41,24 +44,50 @@ def dq_at(dq, i):
             i -= 1
     return None
 
+
 def deliverMessage(msg, sender, reciever):
     requests.post(reciever, msg)
+
 
 @app.route("/messages/deliver/<n>", methods=['POST'])
 def deliverN(n):
     n = int(n)
     while n > 0:
-        earliest_counter = message_counter
-        earliest_msg = {}
-        for key in queues:
-            q = queues[key]
-            msg = dq_at(q, 0)
-            if msg and earliest_counter > msg[0]:
-                earliest_counter = msg[0]
-                earliest_msg = msg[1]
-        deliverMessage(earliest_msg["payload"], earliest_msg["from"], earliest_msg["to"])
-        n -= 1
+        if (queues):
+            earliest_key = list(queues.keys())[0]
+            earliest_counter = message_counter
+            for key in queues:
+                q = queues[key]
+                if (q):
+                    msg = dq_at(q, 0)
+                    if msg and earliest_counter > msg[0]:
+                        earliest_counter = msg[0]
+                        earliest_key = key
+            q = queues[earliest_key]
+            if (q):
+                earliest_msg = q.popleft()[1]
+                deliverMessage(earliest_msg["payload"], earliest_msg["from"], earliest_msg["to"])
+                n -= 1
+            else:
+                break
+        else:
+            break
     return getMessages()
+
+
+@app.route("/messages/deliver/each_pair/<n>", methods=['POST'])
+def deliverN_each_pair(n):
+    pass
+
+
+@app.route("/messages/drop/<id>", methods=['POST'])
+def removeMessage(id):
+    pass
+
+
+@app.route("/messages/reorder/<id1>_<id2>", methods=['POST'])
+def reorderMessage(id1, id2):
+    pass
 
 if __name__ == '__main__':
     app.run()
